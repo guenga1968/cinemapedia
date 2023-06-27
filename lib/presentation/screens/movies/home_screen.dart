52639133dc005e35cba024c1eb6cdd4aeebd1e8d
@guenga1968 <geg1968@hotmail.com>
2023-06-27 12:49:00 -0300

    datasource obteniendo dados 1

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:cinemapedia/config/constants/enviroment.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
